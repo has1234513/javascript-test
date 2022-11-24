0936ec803bce922b837f6de5b028ab96e6e923d8
has1234513 <hashaydar29@gmail.com>
2022-11-24 23:47:13 +0200

    search names for test 1

--- search.js
+++ search.js
@@ -1,31 +1,3 @@
-// $("#search").on("keyup", function() {
-//     var value = $(this).val().toLowerCase();
-//     console.log("keyup")
-//     $("#table tr").filter(function() {
-//       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-//     });
-//   });
-
-//   const searchInput = document.getElementById("search")
-
-//   searchInput?.addEventListener('input', () => {
-//     console.log("input")
-//   })
-
-//   const search = () => {
-//     console.log("input")
-//     $("#searchText").keyup(function(){
-//         _this = this;
-//           $.each($("#searchTable tr"), function() {
-//              if($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1){
-//                  $(this).hide();
-//                }else{
-//                  $(this).show();
-//               }
-//           });
-//     });
-//   } 
-
 $(document).ready(function(){
     $("#searchText").keyup(function(){
         console.log("search")
@@ -41,17 +13,18 @@ $(document).ready(function(){
             }
         });
         
-        $.each(customers, function( key, value ) {
-            $('searchTable').append(
-                '<tr>' +
-                    '<td>' + value.name + ' </td>' +
-                    '<td>' + value.birthdate +'</td>' +
-                '</tr>');
-          });
-        
-          for (var i=0; i<customers.length; i++) {
-            console.log("looped", customers[i])
-          }
-
     });
+
+    
+    
+    for (var i=0; i<customers.length; i++) {
+        console.log("looped", customers[i])
+    
+        $('#searchTable').append(
+        '<tr>' +
+            '<td>' + customers[i].name + ' </td>' +
+            '<td>' + customers[i].birthdate +'</td>' +
+        '</tr>');
+    }
+
 });
