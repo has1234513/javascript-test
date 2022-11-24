a3a2b340dca7ddf86e87d2a1ba2a47a4c779c092
has1234513 <hashaydar29@gmail.com>
2022-11-25 00:22:05 +0200

    solved

---
+++ functions.js
@@ -0,0 +1,60 @@
+
+const getCustomerAge = (cus) => {
+    return new Date().getFullYear() - parseInt(cus.birthdate.split('-')[0])
+}
+
+$(document).ready(function(){
+
+    //loop through customer data
+
+    for (var i=0; i<customers.length; i++) {
+        console.log("age", getCustomerAge(customers[i]))
+        $('#searchTable').append(   
+        '<tr>' +
+            '<td>' + customers[i].name + ' </td>' +
+            '<td>' + customers[i].birthdate +'</td>' +
+        '</tr>');
+    }
+
+    //search function
+
+    $("#searchText").keyup(function(){
+
+        _this = this;
+
+        const text = $(_this).val()
+
+        let newCustomerArray = []
+
+        $.each($("#searchTable tr"), function() {
+            if($(this).text().toLowerCase().indexOf(text.toLowerCase()) === -1){
+                $(this).siblings().children('td').hide()
+            }
+        });
+
+        customers.forEach( c => {
+            if ( c.name.toLocaleLowerCase().includes(text.toLowerCase()) ) {
+                newCustomerArray.push(c)
+            }
+        })
+
+        if (text.includes('-') && parseInt(text.split("-")[0]) !== NaN && parseInt(text.split("-")[1]) !== NaN ) {
+
+            customers.forEach( c => {
+                if ( getCustomerAge(c) > parseInt(text.split("-")[0]) && getCustomerAge(c) < parseInt(text.split("-")[1])) {
+                    newCustomerArray.push(c)
+                }
+            })
+        }
+
+        for (var i=0; i<newCustomerArray.length; i++) {
+            $('#searchTable').append(   
+            '<tr>' +
+                '<td>' + newCustomerArray[i].name + ' </td>' +
+                '<td>' + newCustomerArray[i].birthdate +'</td>' +
+            '</tr>');
+        }
+        
+    });
+
+});
