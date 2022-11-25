6a3593bece4d20c51701864770e4b842c092f5e9
has1234513 <hashaydar29@gmail.com>
2022-11-25 11:22:49 +0200

    fixed empty string bug

--- functions.js
+++ functions.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     //loop through customer data
 
     for (var i=0; i<customers.length; i++) {
-        console.log("age", getCustomerAge(customers[i]))
+        console.log("re")
         $('#searchTable').append(   
         '<tr>' +
             '<td>' + customers[i].name + ' </td>' +
@@ -26,40 +26,44 @@ $(document).ready(function(){
 
         // hides all table elements
 
-        $.each($("#searchTable tr"), function() {
-            if($(this).text().toLowerCase().indexOf(text.toLowerCase()) === -1){
-                $(this).siblings().children('td').hide()
-            }
-        });
-
-        //check ad filter for names
-
-        customers.forEach( c => {
-            if ( c.name.toLocaleLowerCase().includes(text.toLowerCase()) ) {
-                newCustomerArray.push(c)
-            }
-        })
-
-        //check and filter for age group
-
-        if (text.includes('-') && parseInt(text.split("-")[0]) !== NaN && parseInt(text.split("-")[1]) !== NaN ) {
-
+        if (text !== '') {
+            $.each($("#searchTable tr"), function() {
+                if($(this).text().toLowerCase().indexOf(text.toLowerCase()) === -1){
+                    $(this).siblings().children('td').hide()
+                }
+            });
+    
+            //check ad filter for names
+    
             customers.forEach( c => {
-                if ( getCustomerAge(c) > parseInt(text.split("-")[0]) && getCustomerAge(c) < parseInt(text.split("-")[1])) {
+                if ( c.name.toLocaleLowerCase().includes(text.toLowerCase()) ) {
                     newCustomerArray.push(c)
                 }
             })
+    
+            //check and filter for age group
+    
+            if (text.includes('-') && parseInt(text.split("-")[0]) !== NaN && parseInt(text.split("-")[1]) !== NaN ) {
+    
+                customers.forEach( c => {
+                    if ( getCustomerAge(c) > parseInt(text.split("-")[0]) && getCustomerAge(c) < parseInt(text.split("-")[1]) && !newCustomerArray.some(cu => cu.name === c.name)) {
+                        newCustomerArray.push(c)
+                    }
+                })
+            }
+    
+            //re add elements
+            
+            for (var i=0; i<newCustomerArray.length; i++) {
+                $('#searchTable').append(   
+                '<tr>' +
+                    '<td>' + newCustomerArray[i].name + ' </td>' +
+                    '<td>' + newCustomerArray[i].birthdate +'</td>' +
+                '</tr>');
+            }
         }
 
-        //re add elements
         
-        for (var i=0; i<newCustomerArray.length; i++) {
-            $('#searchTable').append(   
-            '<tr>' +
-                '<td>' + newCustomerArray[i].name + ' </td>' +
-                '<td>' + newCustomerArray[i].birthdate +'</td>' +
-            '</tr>');
-        }
         
     });
 
